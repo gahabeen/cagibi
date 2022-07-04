c9b50be07009264f55a517f7c1455218980e699f
Gabin Desserprit <desserprit.gabin@gmail.com>
2022-07-04 13:59:46 +0200

    Fix import

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,6 @@ export * from './core';
 export * from './io';
 export * as Context from './context';
 export * as Symbols from './symbols';
-export * as CONSTS from './consts';
 export * as utils from './utils';
 
 export default { make, stitch, write, read };
